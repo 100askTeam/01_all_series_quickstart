32b39a45a00eb51d3331668699565df4c980492c
weidongshan <weidongshan@qq.com>
2019-09-30 18:48:14 +0800

    01_全系列视频第1季之快速入门/01_嵌入式Linux应用开发基础知识/01.HelloWorld背后没那么简单.mp4

---
+++ 04_快速入门(正式开始)/01_嵌入式Linux应用开发基础知识/source/01_hello/hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/* 执行命令: ./hello weidongshan 
+ * argc = 2
+ * argv[0] = ./hello
+ * argv[1] = weidongshan
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc >= 2)
+		printf("Hello, %s!\n", argv[1]);
+	else
+		printf("Hello, world!\n");
+	return 0;
+}
+
+
