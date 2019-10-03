daa9ae89770bcf427853a228ab0edf847dbb01ed
weidongshan <weidongshan@qq.com>
2019-10-03 22:33:43 +0800

    添加：01_嵌入式Linux应用开发基础知识/03.Makefile规则与示例.mp4

---
+++ 04_快速入门(正式开始)/01_嵌入式Linux应用开发基础知识/source/04_test_Makefile/main.c
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+
+extern void sub_fun(void);
+
+int main(int argc, char* argv[])
+{
+    printf("Main fun!\n");
+    sub_fun();
+    return 0;
+}
---
+++ 04_快速入门(正式开始)/01_嵌入式Linux应用开发基础知识/source/04_test_Makefile/sub.h
@@ -0,0 +1,2 @@
+#define A  1
+void sub_fun(void); 
