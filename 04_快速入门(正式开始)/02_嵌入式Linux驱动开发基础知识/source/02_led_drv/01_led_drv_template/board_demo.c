384d7a66c63433b00bfe8c5a642234829c784d2e
weidongshan <weidongshan@qq.com>
2019-10-12 20:51:33 +0800

    添加：02_嵌入式Linux驱动开发基础知识/06.LED驱动程序框架.mp4

---
+++ 04_快速入门(正式开始)/02_嵌入式Linux驱动开发基础知识/source/02_led_drv/01_led_drv_template/board_demo.c
@@ -0,0 +1,41 @@
+#include <linux/module.h>
+
+#include <linux/fs.h>
+#include <linux/errno.h>
+#include <linux/miscdevice.h>
+#include <linux/kernel.h>
+#include <linux/major.h>
+#include <linux/mutex.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/stat.h>
+#include <linux/init.h>
+#include <linux/device.h>
+#include <linux/tty.h>
+#include <linux/kmod.h>
+#include <linux/gfp.h>
+#include "led_opr.h"
+
+static int board_demo_led_init (int which) /* 初始化LED, which-哪个LED */	   
+{
+	
+	printk("%s %s line %d, led %d\n", __FILE__, __FUNCTION__, __LINE__, which);
+	return 0;
+}
+
+static int board_demo_led_ctl (int which, char status) /* 控制LED, which-哪个LED, status:1-亮,0-灭 */
+{
+	printk("%s %s line %d, led %d, %s\n", __FILE__, __FUNCTION__, __LINE__, which, status ? "on" : "off");
+	return 0;
+}
+
+static struct led_operations board_demo_led_opr = {
+	.init = board_demo_led_init,
+	.ctl  = board_demo_led_ctl,
+};
+
+struct led_operations *get_board_led_opr(void)
+{
+	return &board_demo_led_opr;
+}
+
